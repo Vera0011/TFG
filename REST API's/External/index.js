db98edb69462847a90edcad6cfcab22599e7e4e9
Vera0011 <contact.vera0011@gmail.com>
2023-11-10 16:08:53 +0100

    Added external API

---
+++ REST API's/External/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+const ads = [
+  {title: 'Hello, world (again)!'}
+];
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.send(ads);
+});
+
+// starting the server
+app.listen(3001, () => {
+  console.log('listening on port 3001');
+});
